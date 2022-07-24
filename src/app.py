2ca50c796c67dc92c26614ca4c611131899dd7ba
Spartak Karapetian <s.karapetian@adtelligent.com>
2022-07-24 16:06:49 +0200

    Last update

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 from unicodedata import name
 from flask import Flask
 
-app= Flask(__name__)
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
